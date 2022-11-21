c36ed880a68384a104da9c7b7d770db6efdc9c18
oliver-or-not <stellaria700@gmail.com>
2022-11-21 15:18:02 +0900

    Initial Commit

---
+++ Comet Monster Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Comet Monster Demo
+//
+//  Created by Wonil Lee on 2022/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
